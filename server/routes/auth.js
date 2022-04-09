0f8d5f7dd8b9df86b7d39acb58da1cdbd13d2364
Rifa-K-Malik <malikrif07@gmail.com>
2022-04-09 13:01:20 +0530

    Saving data in database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,41 @@
 import express from "express";
+import mongoose from "mongoose";
+import '../../server/models/user.js';
 
 const router = express.Router();
+const User = mongoose.model("User");
 
 router.get('/', (req, res) => {
     res.send("Hello")
 });
 
 router.post('/signup', (req, res) => {
-    res.send("Hello SIGUP")
+    const { name, email, password } = req.body;
+    if (!email || !name || !password) {
+        return res.status(422).json({ error: "Please add all details" })
+    }
+    User.findOne({ email: email })
+        .then((savedUseer) => {
+            if (savedUseer) {
+                return res.status(422).json({ error: "Email exists" })
+
+            }
+            const user = new User({
+                email,
+                password,
+                name
+            })
+            user.save()
+                .then(user => {
+                    res.json({ message: "saved successfully" })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+        .catch()
 });
 
-export default  router;
+export default router;
 
 
